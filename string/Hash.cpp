71ee1340be28eb4d95feed65e9aa2bd2639fd90e
Jengle88 <45243533+Jengle88@users.noreply.github.com>
2019-12-14 22:28:01 +0300

    Update Hash.cpp

--- string/Hash.cpp
+++ string/Hash.cpp
@@ -1,1 +1,115 @@
+//Goodbye stupid girls & Hello L0u1Za, Codeforces and my friends(nobody)
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <map>
+#include <set>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <utility>
+#include <type_traits>
+#include <climits>
+#include <functional>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+//#include <Windows.h>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+template <class T>
+using pt = std::pair<T, T>;
+using namespace std;
 
+class _hash
+{
+private:
+	int x, m;//basa, mod
+	vector<int> power, prefix;//step x, tabl
+
+public:
+	_hash(const string& s, int x = 73, int m = 1e9+7) : x(x), m(m)//build hash
+	{
+		power.resize(s.size() + 1);
+		prefix.resize(s.size() + 1);
+
+		power[0] = 1;
+		for (int i = 1; i < s.size(); i++)
+		{
+			power[i] = (ll)power[i - 1] * x % m;
+		}
+		for (int i = 0; i <= s.size() - 1; i++)
+		{
+			prefix[i + 1] = ((ll)prefix[i] * x + s[i] - 'a' + 1) % m;
+		}
+	}
+	int get_size()// size prefix
+	{
+		return prefix.size();
+	}
+	int get_hashp(int i, int j)//hash on substr
+	{
+		return (prefix[j] - (ll)prefix[i - 1] * power[j - i + 1] % m + m) % m;
+	}
+	int get_hashf(string &s)//hash func
+	{
+		int res = 0;
+		for (auto c : s)
+		{
+			res = ((ll)res * x + c - 'a' + 1) % m;
+		}
+		return res;
+	}
+};
+
+void solve()//example
+{
+	string s, s1;
+	cin >> s;
+	_hash hf(s);
+	int q;
+	cin >> q;
+flag:
+	while (q--)
+	{
+		cin >> s1;
+		int fnd = hf.get_hashf(s1);
+		if (s1.size() == s.size() && fnd == hf.get_hashp(1, hf.get_size() - 1))
+		{
+			cout << "YES\n";
+			goto flag;
+		}
+		for (int i = 1; i <= s.size() - s1.size(); i++)
+		{
+			if (hf.get_hashp(i, i + s1.size() - 1) == fnd)
+			{
+				cout << "YES\n";
+				goto flag;
+			}
+		}
+		cout << "NO\n";
+	}
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+#else
+#endif
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	solve();
+	return 0;
+}
