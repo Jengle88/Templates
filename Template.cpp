d5f06bcb449ae260ef28e303d2a2a0125b50e6a9
Jengle88 <45243533+Jengle88@users.noreply.github.com>
2020-08-24 18:47:37 +0300

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+#pragma GCC optimize("section-anchors")
+#pragma GCC optimize("profile-values,profile-reorder-functions,tracer")
+#pragma GCC optimize("vpt")
+#pragma GCC optimize("rename-registers")
+#pragma GCC optimize("move-loop-invariants")
+#pragma GCC optimize("unswitch-loops")
+#pragma GCC optimize("function-sections")
+#pragma GCC optimize("data-sections")
+#pragma GCC optimize("branch-target-load-optimize")
+#pragma GCC optimize("branch-target-load-optimize2")
+#pragma GCC optimize("btr-bb-exclusive")
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <map>
+#include <set>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include <cmath>
+#include <cstdio>
+#include <utility>
+#include <climits>
+#include <functional>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <type_traits>
+#include <deque>
+#include <unordered_map>
+//#include <Windows.h>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+template <class T>
+using pt = std::pair<T, T>;
+using namespace std;
+
+
+int main() {
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		//freopen("output.txt", "w", stdout);
+	#else
+	#endif
+	//freopen("input.txt", "r", stdin);
+	//freopen("input1.txt", "w", stdout);
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	return 0;
+}
