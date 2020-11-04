85cec0b7bbcf2344bb911f7d1851b58549605061
Jengle88 <45243533+Jengle88@users.noreply.github.com>
2020-11-04 21:35:55 +0300

    new vers

--- struct/SegmentTree.cpp
+++ struct/SegmentTree.cpp
@@ -1,5 +1,5 @@
-//Goodbye stupid girls & Hello L0u1Za, Codeforces and my friends(nobody)
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -21,95 +21,96 @@
 #include <type_traits>
 #include <deque>
 #include <unordered_map>
+
 //#include <Windows.h>
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
-template <class T>
+template<class T>
 using pt = std::pair<T, T>;
+#define INF 1e9
 using namespace std;
+
 
 struct hoba
 {
 	hoba *l = nullptr, *r = nullptr;
-	ll sum = 0, del = 0;
-	int lf, rg;
-};
-vector<ll> a;
+	int lf = 0, rg = 0;
+	int sum = 0, min = INF, max = -INF;
 
-hoba* build(int l, int r)
+	hoba(int l, int r) : lf(l), rg(r) {};
+};
+
+vector<int> a;
+
+hoba *build(int l, int r)
 {
-	hoba *root = new hoba;
+	hoba *root = new hoba(l, r);
 	if (l == r)
 	{
-		root->lf = root->rg = l;
-		root->sum = a[l];
+		root->max = root->min = root->sum = a[l];
 		return root;
 	}
-	root->lf = l, root->rg = r;
-	root->l = build(l, (r + l) / 2);
-	root->r = build((r + l) / 2 + 1, r);
+	root->l = build(l, (l + r) / 2);
+	root->r = build((l + r) / 2 + 1, r);
 	root->sum = root->l->sum + root->r->sum;
+	root->min = min(root->l->min, root->r->min);
+	root->max = max(root->l->max, root->r->max);
 	return root;
 }
 
-void update_d(hoba *root, int l, int r, int d)
+void update(hoba *root, int ind, int d)//1 elem
 {
-	if (root->lf > r || root->rg < l)
+	if (root->lf > ind || root->rg < ind)
 		return;
-	if (root->lf >= l && root->rg <= r)
+	if (root->lf == root->rg && root->rg == ind)
 	{
-		root->del += d * (root->rg - root->lf + 1);
-		return;
+		root->sum = root->min = root->max = d;
 	}
-	update_d(root->l, l, r, d);
-	update_d(root->r, l, r, d);
-	root->del = root->l->del + root->r->del;
-	return;
+	else
+	{
+		update(root->l, ind, d);
+		update(root->r, ind, d);
+		root->min = min(root->l->min, root->r->min);
+		root->max = max(root->l->max, root->r->max);
+		root->sum = root->l->sum + root->r->sum;
+	}
 }
 
-void update_x(hoba *root, int l, int r, int x)//not ready
-{
-	if (root->lf > r || root->rg < l)
-		return;
-	if (root->lf >= l && root->rg <= r)
-	{	
-		root->sum = x * (root->rg - root->lf + 1);
-		return;
-	}
-	update_x(root->l, l, r, x);
-	update_x(root->r, l, r, x);
-	root->sum = root->r->sum + root->l->sum;
-	return;
-}
-
-int query(hoba *root, int l, int r)
+int query_sum(hoba *root, int l, int r)
 {
 	if (root->lf > r || root->rg < l)
 		return 0;
-	if (root->lf >= l && root->rg <= r)
-	{
-		return root->sum + root->del;
-	}
-	return query(root->l, l, r) + query(root->r, l, r);
+	if (l <= root->lf && root->rg <= r)
+		return root->sum;
+	return query_sum(root->l, l, r) + query_sum(root->r, l, r);
 }
 
-int main() {
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-#else
-#endif
-	//freopen("input.txt", "r", stdin);
-	//freopen("input2.txt", "w", stdout);
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	int n;
-	cin >> n;
-	a.resize(n);
-	for (int i = 0; i < n; i++)	cin >> a[i];
-	hoba *root = new hoba;
-	root = build(0, n - 1);
+int query_min(hoba *root, int l, int r)
+{
+	if (root->lf > r || root->rg < l)
+		return INF;
+	if (l <= root->lf && root->rg <= r)
+		return root->min;
+	return min(query_min(root->l, l, r), query_min(root->r, l, r));
+}
 
+int query_max(hoba *root, int l, int r)
+{
+	if (root->lf > r || root->rg < l)
+		return -INF;
+	if (l <= root->lf && root->rg <= r)
+		return root->max;
+	return max(query_max(root->l, l, r), query_max(root->r, l, r));
+}
+
+int main()
+{
+	a = {3, 1, 8, 5, 9, 3, 2, 6, 4, 5, 1, 12, 213, 12, 1, 23, 123, 123, 12, 3, 23, 123};
+	hoba *tree = build(0, a.size() - 1);
+	update(tree, a.size() / 2, 10);
+	cout << query_min(tree, 0, a.size() - 1) << '\n';
+	cout << query_max(tree, 0, a.size() - 1) << '\n';
+	cout << query_sum(tree, 0, a.size() - 1) << '\n';
 	return 0;
 }
