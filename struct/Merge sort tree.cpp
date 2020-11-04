e6278344250e62daaac0f66b17c85554880f238f
Jengle88 <45243533+Jengle88@users.noreply.github.com>
2020-11-04 22:58:36 +0300

    Update Merge sort tree.cpp

--- struct/Merge sort tree.cpp
+++ struct/Merge sort tree.cpp
@@ -1,5 +1,5 @@
-//Goodbye stupid girls & Hello L0u1Za, Codeforces and my friends(nobody)
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -21,71 +21,57 @@
 #include <type_traits>
 #include <deque>
 #include <unordered_map>
+
 //#include <Windows.h>
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
-template <class T>
+template<class T>
 using pt = std::pair<T, T>;
+#define INF 1e9
 using namespace std;
+
+vector<int> a;
 
 struct hoba
 {
 	hoba *l = nullptr, *r = nullptr;
-	vector<int> a;
 	int lf, rg;
+	vector<int> a;
+
+	hoba(int l, int r) : lf(l), rg(r) {};
 };
 
-vector<int> a;
-
-hoba* build(int l, int r)
+hoba *build(int l, int r)
 {
-	hoba * root = new hoba;
+	hoba *root = new hoba(l, r);
 	if (l == r)
 	{
-		root->lf = root->rg = l;
 		root->a.push_back(a[l]);
 		return root;
 	}
-	root->lf = l; root->rg = r;
-	root->l = build(l, (r+l) / 2);
-	root->r = build((r+l) / 2 + 1, r);
+	root->l = build(l, (r + l) / 2);
+	root->r = build((r + l) / 2 + 1, r);
 	root->a.resize(root->l->a.size() + root->r->a.size());
 	merge(root->l->a.begin(), root->l->a.end(), root->r->a.begin(), root->r->a.end(), root->a.begin());
 	return root;
 }
 
-int query(hoba *root, int l, int r, int x)// greater than x
+int query(hoba *root, int l, int r, int x)
 {
-	if (l > root->rg || r < root->lf)
+	if (r < root->lf || l > root->rg)
 		return 0;
-
-	if (l >= root->lf && r <= root->rg)
-		return upper_bound(root->a.begin(), root->a.end(), x) - lower_bound(root->a.begin(),root->a.end(),x);
-
+	if (l <= root->lf && root->rg <= r)
+		return upper_bound(root->a.begin(), root->a.end(), x) -
+			   lower_bound(root->a.begin(), root->a.end(), x); // x replace 0, if [0,x]
 	return query(root->l, l, r, x) + query(root->r, l, r, x);
-
 }
 
-int main() {
-/*#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	//freopen("output.txt", "w", stdout);
-#else
-#endif*/
-	freopen("input.txt", "r", stdin);
-	//freopen("input1.txt", "w", stdout);
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	int n;
-	cin >> n;
-	a.resize(n);
-	for (int i = 0; i < n; i++)
-	{
-		cin >> a[i];
-	}
-	hoba * root = new hoba;
-	root = build(0, n-1);
 
+int main()
+{
+	a = {3, 1, 8, 5, 9, 3, 2, 6, 4, 5, 1, 12, 213, 12, 1, 23, 123, 123, 12, 3, 23, 123};
+	hoba *tree = build(0, a.size() - 1);
+	cout << query(tree,0,a.size(), 3);
 	return 0;
 }
